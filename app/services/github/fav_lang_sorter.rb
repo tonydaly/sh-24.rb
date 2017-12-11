0fa4016a9d8d143a20c1d0cb69710373124da436
Tony Daly <tony.daly@me.com>
2017-12-11 18:46:31 +0000

    Small comment.

--- app/services/github/fav_lang_sorter.rb
+++ app/services/github/fav_lang_sorter.rb
@@ -4,6 +4,7 @@ module Github
       @repos = repos
     end
 
+    # TODO: handle repos that don't have a language
     def call
       repos_count = @repos.reduce(Hash.new(0)) { |acc, repo| acc[repo.language] += 1; acc }
       Hash[repos_count.sort_by{ |k, v| v }.reverse]
